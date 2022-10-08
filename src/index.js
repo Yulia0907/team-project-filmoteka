9c9238715c974d13cd2ba589cd602ff80aa2be0e
NilKad <kadulin@gmail.com>
2022-10-08 21:49:21 +0300

    4
    отключаю модалки

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import moviesGallery from './js/moviesGallery';
 const currentPage = 1;
 
 // import './js/footer';
-import './js/teamLightbox';
-import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+// import './js/teamLightbox';
+// import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
