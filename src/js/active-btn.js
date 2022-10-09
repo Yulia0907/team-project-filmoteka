fa7176df0e515027bf11ef93afd9c1a0239a634f
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-09 15:21:59 +0300

    Update active-btn.js

--- src/js/active-btn.js
+++ src/js/active-btn.js
@@ -4,50 +4,9 @@ const watchedSet = document.querySelector('.header-button__wrapper');
 // console.log(watchedBtn);
 // console.log(queueBtn);
 // watchedBtn.disabled = true;
-watchedSet.addEventListener(
-  'click',
-  makeActiveWatched,
-  makeActiveQueue,
-  activeBtn
-);
+watchedSet.addEventListener('click', makeActiveWatched);
 
-function activeBtn(e) {
-  if (watchedBtn.classList.contains('is-active') === true) {
-    watchedBtn.disabled = true;
-    queueBtn.disabled = false;
-  } else if (queueBtn.classList.contains('is-active') === true) {
-    queueBtn.disabled = true;
-    watchedBtn.disabled = false;
-  }
-}
-
-function makeActiveWatched(e) {
-  e.preventDefault();
+function makeActiveWatched() {
   watchedBtn.classList.toggle('is-active');
-  // if (watchedBtn.classList.contains('is-active') === true) {
-  //   watchedBtn.disabled = true;
-  //   queueBtn.disabled = false;
-}
-// watchedBtn.disabled = true;
-// queueBtn.disabled = false;
-// }
-
-function makeActiveQueue(e) {
-  e.preventDefault();
   queueBtn.classList.toggle('is-active');
-  // if (queueBtn.classList.contains('is-active') === true) {
-  //   queueBtn.disabled = true;
-  //   watchedBtn.disabled = false;
-  // }
 }
-//   watchedBtn.disabled = false;
-//   queueBtn.disabled = true;
-// }
-// if (watchedBtn.classList.contains('is-active')) {
-//   watchedBtn.disabled = true;
-//   queueBtn.disabled = false;
-// }
-// if (queueBtn.classList.contains('is-active')) {
-//   queueBtn.disabled = true;
-//   watchedBtn.disabled = false;
-// }
