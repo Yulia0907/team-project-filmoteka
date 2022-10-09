5269fe66081539cc0a88c2de2bb0492a992fb6be
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-09 17:32:35 +0300

    Update active-btn.js

--- src/js/active-btn.js
+++ src/js/active-btn.js
@@ -2,21 +2,13 @@ const watchedBtn = document.querySelector('[data-id=watched-btn]');
 const queueBtn = document.querySelector('[data-id=queue-btn]');
 const watchedSet = document.querySelector('.header-button__wrapper');
 
-watchedBtn.disabled = true;
-
 watchedSet.addEventListener('click', makeActiveBtn);
 
-function makeActiveBtn() {
+function makeActiveBtn(evt) {
+  if (evt.target.classList.contains('is-active')) {
+    return;
+  }
   watchedBtn.classList.toggle('is-active');
   queueBtn.classList.toggle('is-active');
-  if (watchedBtn.classList.contains('is-active') === true) {
-    watchedBtn.disabled = true;
-  } else {
-    watchedBtn.disabled = false;
-  }
-  if (queueBtn.classList.contains('is-active') === true) {
-    queueBtn.disabled = true;
-  } else {
-    queueBtn.disabled = false;
-  }
+  return;
 }
