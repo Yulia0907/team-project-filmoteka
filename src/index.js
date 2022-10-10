2d1419c37f69b49035221fcab51615e173f2b3af
Yelyzaveta Demchenko <97131904+yelyzaveta05@users.noreply.github.com>
2022-10-10 05:27:15 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import paginations from './js/paginations';
 
 const currentPage = 1;
+import * as modalRegistration from './js/modal-registr'
--- src/js/modal-registr.js
+++ src/js/modal-registr.js
@@ -1,17 +1,46 @@
-const modalSignUpEl = document.querySelector('#modal-singup');
-const modalLogInEl = document.querySelector('#modal-welcome');
+import * as basicLightbox from 'basiclightbox'
+import 'basiclightbox/dist/basicLightbox.min.css';
+
 const openModalSignUpEl = document.querySelector('#modal-singup-btn');
 const openModalLogInEl = document.querySelector('#modal-login-btn');
+
+const openModal = document.querySelector('#check');
+openModal.addEventListener("click", toggleModal);
+
+const markupModalLogIn = basicLightbox.create(document.querySelector('#modal-welcome'));
+
+function toggleModal() {
+    markupModalLogIn.show();
+
+    window.addEventListener('keydown', onEscKeyPress);
+    function onEscKeyPress(e) {
+        if (e.code === "Escape") {
+            markupModalLogIn.close();
+        }
+    }
+};
 
 openModalSignUpEl.addEventListener('click', onSignUp);
 openModalLogInEl.addEventListener('click', onLogIn);
 
+const markupModalSignUp = basicLightbox.create(document.querySelector('#modal-singup'));
 function onSignUp() {
-    modalSignUpEl.classList.toggle('is-hidden');
-    modalLogInEl.classList.toggle('is-hidden');
+
+    markupModalSignUp.show();
+
+    window.addEventListener('keydown', onEscKeyPress);
+    function onEscKeyPress(e) {
+        if (e.code === "Escape") {
+            markupModalSignUp.close();
+        }
+    }
+
+    markupModalLogIn.close();
 };
 
 function onLogIn() {
-    modalSignUpEl.classList.toggle('is-hidden');
-    modalLogInEl.classList.toggle('is-hidden');
+    markupModalLogIn.show();
+    markupModalSignUp.close();
 };
+
+export { toggleModal, onSignUp, onLogIn };
