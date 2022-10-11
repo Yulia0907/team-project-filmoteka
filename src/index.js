895ccc1e06503d7ee0a17931f21046d38351bfc5
Artem-Borzov <abug990@gmail.com>
2022-10-11 16:13:32 +0300

    resolving conflicts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/scroll.js';
 import './js/moviesGallery';
 import './js/form/form-search-input';
 import './js/modal-registr';
-
+import './js/firebase';
 const currentPage = 1;
 
 import './js/footer';
