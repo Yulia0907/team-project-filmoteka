7a325f073cbe6f8aeb69fb351dd51913f174c00d
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-10-11 17:16:00 +0300

    Update my-library.js

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -27,7 +27,7 @@ function onWatchedBtnClick() {
   if (watchedBtnEl.classList.contains('is-active')) {
     return;
   }
-  const getWatched = localStorageApi.getData('watched'));
+  const getWatched = localStorageApi.getData('watched');
   const markup = createMovieCards(getWatched);
   moviesContainer.innerHTML = markup;
 }
@@ -47,30 +47,30 @@ moviesContainer.addEventListener('click', onMovieCardClick2);
 // как сделать функцию с параметром и обработчиком event?
 
 export async function onMovieCardClick2(e) {
-    const targetFilm = e.target.closest('li').dataset.id; // id текущего фильма при открытии модалки
-    console.log(targetFilm);
-    if (e.target.nodeName === 'UL') {
-      return;
-    }
-    try {
-      const movies = localStorageApi.getData('watched'); // забираем фильмы из Local Storage по тегу "movies"
-      const parsedGenres = localStorageApi.getData('genresList'); // забираем жанры из Local Storage
-  
-      const film = movies.filter(({ id }) => id === Number(targetFilm))[0]; // метод filter возвращает массив, поэтому берем элемент этого массива
-      const { genre_ids } = film;
-      let genres;
-      if (genre_ids) {
-        genres = parsedGenres
-          .filter(({ id }) => genre_ids.includes(id)) // перебираем массив всех жанров по id и возвращаем только с текущими
-          .map(({ name }) => name); //перебираем массив текущих id и возвращаем массив с именами
-      }
-      film.genres = genres; // в ключ текущего объекта film сохраняем жанры по именам
-  
-      localStorageApi.setData('current-film', film);
-  
-      modalBasicLightbox(film);
-      localStorageApi.addListenersToBtns();
-    } catch (error) {
-      console.log(error.message);
-    }
+  const targetFilm = e.target.closest('li').dataset.id; // id текущего фильма при открытии модалки
+  console.log(targetFilm);
+  if (e.target.nodeName === 'UL') {
+    return;
   }
+  try {
+    const movies = localStorageApi.getData('watched'); // забираем фильмы из Local Storage по тегу "movies"
+    const parsedGenres = localStorageApi.getData('genresList'); // забираем жанры из Local Storage
+
+    const film = movies.filter(({ id }) => id === Number(targetFilm))[0]; // метод filter возвращает массив, поэтому берем элемент этого массива
+    const { genre_ids } = film;
+    let genres;
+    if (genre_ids) {
+      genres = parsedGenres
+        .filter(({ id }) => genre_ids.includes(id)) // перебираем массив всех жанров по id и возвращаем только с текущими
+        .map(({ name }) => name); //перебираем массив текущих id и возвращаем массив с именами
+    }
+    film.genres = genres; // в ключ текущего объекта film сохраняем жанры по именам
+
+    localStorageApi.setData('current-film', film);
+
+    modalBasicLightbox(film);
+    localStorageApi.addListenersToBtns();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
