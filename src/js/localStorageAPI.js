87aa3041f1f17ab88b0a573c82b02e49f21dadb0
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-10-12 18:50:24 +0300

    changes

--- src/js/localStorageAPI.js
+++ src/js/localStorageAPI.js
@@ -21,24 +21,39 @@ export class localStorageAPI {
 
   addListenersToBtns = () => {
     this.currentMovie = this.getData('current-film');
+    this.watchedMoviesList = this.getData('watched');
+
+    if (this.watchedMoviesList === null) {
+      // при удалении всех записей, чтоб список был непустым,
+      // так как кнопка Delete последнего оставшегося элемента удаляет и сам тэг из Local Storage
+      this.watchedMoviesList = [];
+    }
+
     this.watchedBtn = document.querySelector('.btn-watched');
     this.queueBtn = document.querySelector('.btn-queue');
-    this.watchedBtn.addEventListener('click', this.onClickWatched);
-    this.queueBtn.addEventListener('click', this.onClickQueue);
 
+    // для кнонпки Add to Watched
     if (
       this.watchedMoviesList.map(({ id }) => id).includes(this.currentMovie.id)
     ) {
-      // this.watchedBtn.classList.add('added');
+      this.watchedBtn.classList.toggle('added'); // чтоб при повторном открытии модалки одного и того же фильма сохранялся статус кнопки
       this.watchedBtn.innerText = 'Delete';
       this.watchedBtn.addEventListener('click', this.onClickWatchedDelete);
+    } else {
+      this.watchedBtn.innerText = 'Add to watched';
+      this.watchedBtn.addEventListener('click', this.onClickWatched);
     }
 
+    // для кнонпки Add to Queue
     if (
       this.queueMoviesList.map(({ id }) => id).includes(this.currentMovie.id)
     ) {
+      this.queueBtn.classList.toggle('added');
       this.queueBtn.innerText = 'Delete';
       this.queueBtn.addEventListener('click', this.onClickQueueDelete);
+    } else {
+      this.queueBtn.innerText = 'Add to queue';
+      this.queueBtn.addEventListener('click', this.onClickQueue);
     }
   };
 
@@ -46,74 +61,71 @@ export class localStorageAPI {
     this.watchedBtn.innerText = 'Delete';
     this.watchedBtn.classList.toggle('added');
 
-    if (this.watchedBtn.classList.contains('added')) {
-      this.watchedBtn.addEventListener('click', this.onClickWatchedDelete);
+    if (this.getData('watched')) {
+      this.watchedMoviesList = this.getData('watched');
     }
 
-    if (this.watchedMoviesIds.includes(this.currentMovie.id)) {
-      return;
-    } else {
-      this.watchedMoviesIds.push(this.currentMovie.id);
-      this.watchedMoviesList.push(this.currentMovie);
-      // console.log(this.watchedMoviesList);
-    }
+    this.watchedMoviesList.push(this.currentMovie);
     this.setData('watched', this.watchedMoviesList);
-    this.setData('current-film', this.currentMovie);
+
+    this.watchedBtn.removeEventListener('click', this.onClickWatched);
+    this.watchedBtn.addEventListener('click', this.onClickWatchedDelete);
   };
 
   onClickQueue = () => {
     this.queueBtn.innerText = 'Delete';
     this.queueBtn.classList.toggle('added');
 
-    if (this.queueBtn.classList.contains('added')) {
-      this.queueBtn.addEventListener('click', this.onClickQueueDelete);
+    if (this.getData('queue')) {
+      this.queueMoviesList = this.getData('queue');
     }
 
-    if (this.queueMoviesIds.includes(this.currentMovie.id)) {
-      return;
-    } else {
-      this.queueMoviesIds.push(this.currentMovie.id);
-      this.queueMoviesList.push(this.currentMovie);
-    }
+    this.queueMoviesList.push(this.currentMovie);
     this.setData('queue', this.queueMoviesList);
-    this.setData('current-film', this.currentMovie);
+
+    this.queueBtn.removeEventListener('click', this.onClickQueue);
+    this.queueBtn.addEventListener('click', this.onClickQueueDelete);
   };
 
   onClickWatchedDelete = () => {
+    this.watchedBtn.classList.toggle('added');
     this.watchedBtn.innerText = 'Add to watched';
 
     this.watchedMoviesList = this.getData('watched');
     this.currentMovie = this.getData('current-film');
 
     const items = this.watchedMoviesList.map(({ id }) => id);
-    if (items.includes(this.currentMovie.id)) {
-      const itemToDelete = items.indexOf(this.currentMovie.id);
-      this.watchedMoviesList.splice(itemToDelete, 1);
 
-      this.setData('watched', this.watchedMoviesList);
+    const itemToDelete = items.indexOf(this.currentMovie.id);
+    this.watchedMoviesList.splice(itemToDelete, 1);
 
-      if (this.watchedMoviesList.length === 0) {
-        localStorage.removeItem('watched');
-      }
+    this.setData('watched', this.watchedMoviesList);
+
+    if (this.watchedMoviesList.length === 0) {
+      localStorage.removeItem('watched');
     }
+    this.watchedBtn.removeEventListener('click', this.onClickWatchedDelete);
+    this.watchedBtn.addEventListener('click', this.onClickWatched);
   };
 
   onClickQueueDelete = () => {
+    this.queueBtn.classList.toggle('added');
     this.queueBtn.innerText = 'Add to queue';
 
     this.queueMoviesList = this.getData('queue');
     this.currentMovie = this.getData('current-film');
 
     const items = this.queueMoviesList.map(({ id }) => id);
-    if (items.includes(this.currentMovie.id)) {
-      const itemToDelete = items.indexOf(this.currentMovie.id);
-      this.queueMoviesList.splice(itemToDelete, 1);
 
-      this.setData('queue', this.queueMoviesList);
+    const itemToDelete = items.indexOf(this.currentMovie.id);
+    this.queueMoviesList.splice(itemToDelete, 1);
 
-      if (this.queueMoviesList.length === 0) {
-        localStorage.removeItem('queue');
-      }
+    this.setData('queue', this.queueMoviesList);
+
+    if (this.queueMoviesList.length === 0) {
+      localStorage.removeItem('queue');
     }
+    this.queueBtn.removeEventListener('click', this.onClickQueueDelete);
+    this.queueBtn.addEventListener('click', this.onClickQueue);
   };
 }
