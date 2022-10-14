dcdf6dddb2972554c90605722981debbe768266b
NilKad <kadulin@gmail.com>
2022-10-14 16:35:55 +0300

    _test_1
    не отобр страница

--- src/js/trendingMovies.js
+++ src/js/trendingMovies.js
@@ -10,23 +10,20 @@ const moviesContainer = document.querySelector('.movies');
  * Function fetch trending movies and make markup on page
  */
 async function trendingMovies() {
-  try {
-    const res = await fetchTrendingMovies();
-    moviesContainer.innerHTML = createMovieCards(res.results);
+  // try {
+  const res = await fetchTrendingMovies();
+  moviesContainer.innerHTML = createMovieCards(res.results);
 
-    const pagination = new Pagination(
-      'pagination',
-      paginationOptions(res.total_results)
-    );
+  const pagination = new Pagination('pagination', paginationOptions(res.total_results));
 
-    pagination.on('afterMove', ({ page }) => {
-      fetchTrendingMovies(page).then(res => {
-        moviesContainer.innerHTML = createMovieCards(res.results);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      });
+  pagination.on('afterMove', ({ page }) => {
+    fetchTrendingMovies(page).then(res => {
+      moviesContainer.innerHTML = createMovieCards(res.results);
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     });
-  } catch (error) {
-    console.log('error', error.message);
-  }
+  });
+  // } catch (error) {
+  //   console.log('error', error.message);
+  // }
 }
 trendingMovies();
